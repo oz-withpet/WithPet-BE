66bc15714c217445d4682877463ae7eb2033af41
chdan-hub <ksmii358@gmail.com>
2025-11-06 23:46:29 -0800

    포스트 urls, veiws파일 생성 및 작성 / config/urls 경로 추가

--- apps/community/posts/views.py
+++ apps/community/posts/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
-from apps.community.posts.services.listing import community_list as _community_list
+from apps.community.posts.services.listing import community_list
 
 class PostsListView(APIView):
     # GET /posts  (view 기본 community)
-    get = _community_list
+    def get(self, request, *args, **kwargs):
+        return community_list(request)
