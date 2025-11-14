8c0b4ceb4449f7925142537af33b79a1f40898a8
Ubuntu <ubuntu@ip-172-31-42-177.ap-northeast-2.compute.internal>
2025-11-14 06:15:14 +0000

    add/mypage/06 urls.py 보완 및 가시성 확립

--- config/urls.py
+++ config/urls.py
@@ -4,21 +4,26 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path, include
 
 urlpatterns = [
+    # --- 1. 관리자, 문서, 토큰 ---
     path("admin/", admin.site.urls),
-    path('', include('apps.users.urls')),
-    path("", include("apps.community.posts.urls")),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),                      # OpenAPI JSON/YAML
-    path('swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),# 내장 Swagger UI
-    path("", include("apps.community.posts.urls")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # --- 2. Community, Users ---
+    path('', include('apps.users.urls')),
+    path("", include("apps.community.posts.urls")),
     path("", include("apps.community.comments.urls")),
     path("", include("apps.community.likes.urls")),
     path("", include("apps.community.reports.urls")),
-    path('', include('apps.maps.urls')),
-    path('mypage/', include('apps.mypage.urls'))
-]
 
+    # --- 3. Mypage ---
+    path('mypage/', include('apps.mypage.urls')),
+
+    # --- 4. Maps ---
+    path('api/', include('apps.maps.urls')),
+]
 
 def trigger_error(request):
     division_by_zero = 1 / 0
