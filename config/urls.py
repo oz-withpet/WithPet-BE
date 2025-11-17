61179bd96a335adcfd90785444c5ef8ea575c67b
chdan-hub <ksmii358@gmail.com>
2025-11-16 22:01:09 -0800

    로컬서버에서 media 서빙 설정

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('apps.users.urls')),
@@ -34,3 +37,6 @@ urlpatterns = [
     path("", include("apps.community.reports.urls")),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
