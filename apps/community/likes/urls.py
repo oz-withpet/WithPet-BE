309409e9396d8fa9356abf8dbf15d06614e4192b
chdan-hub <ksmii358@gmail.com>
2025-11-17 20:14:03 -0800

    좋아요 아이디 통합

--- apps/community/likes/urls.py
+++ apps/community/likes/urls.py
@@ -4,5 +4,5 @@ from .views import PostLikeView
 app_name = "likes"
 
 urlpatterns = [
-    path("posts/<str:post_id>/like", PostLikeView.as_view(), name="post-like"),
+    path("posts/<int:post_id>/like", PostLikeView.as_view(), name="post-like"),
 ]
--- apps/community/likes/views.py
+++ apps/community/likes/views.py
@@ -2,11 +2,11 @@ from rest_framework.views import APIView
 from .services import status as like_status, on as like_on, off as like_off
 
 class PostLikeView(APIView):
-    def get(self, request, post_id: str, *args, **kwargs):
+    def get(self, request, post_id: int, *args, **kwargs):
         return like_status(request, post_id)
 
-    def put(self, request, post_id: str, *args, **kwargs):
+    def put(self, request, post_id: int, *args, **kwargs):
         return like_on(request, post_id)
 
-    def delete(self, request, post_id: str, *args, **kwargs):
+    def delete(self, request, post_id: int, *args, **kwargs):
         return like_off(request, post_id)
